65c63ffd92b68975f6fec798efd1096e76b6ac52
GodspowerJohn <grtsoym@gmail.com>
2026-02-08 04:55:14 +0100

    Fix: Restore correct import in useAuth.js

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,6 +1,6 @@
 // src/hooks/useAuth.js
 import { useContext } from 'react';
-import { AuthContext } from '../hooks/useAuth';
+import { AuthContext } from '../contexts/AuthContext';
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
